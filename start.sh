81e5774a7bdbc0b21ed61efcbc71c79abada12e2
Anthony Kim <anthony.jinhyuk.kim@gmail.com>
2021-03-25 10:14:48 +0900

    apply dynamic profiles

--- start.sh
+++ start.sh
@@ -1,4 +1,52 @@
 #!/bin/bash
 
-sudo kubectl apply -f blog-namespace.yaml
-sudo kubectl apply -f blog-web-service.yaml
+argc=$#
+args=("$@")
+
+function printUsage() {
+    echo "USAGE: /bin/bash start.sh {target: integer value, 0 (local), 1 (dev), 2 (prod)}"
+}
+
+function init() {
+    REGISTRY_ID="hyuk0628"
+
+    if [[ ${argc} -eq 1 ]]; then
+        if [[ ${args[0]} -eq 0 ]]; then
+            echo "Start local profile services and deployments."
+            PROFILE="local"
+            TAG="snapshot"
+        elif [[ ${args[0]} -eq 1 ]]; then
+            echo "Start dev profile services and deployments."
+            PROFILE="dev"
+            TAG="snapshot"
+        elif [[ ${args[0]} -eq 2 ]]; then
+            echo "Start prod profile services and deployments."
+            PROFILE="prod"
+            TAG="release"
+        else
+            printUsage
+            exit 0
+        fi
+    else
+        printUsage
+        exit 0
+    fi
+}
+init
+
+function start() {
+    for file in $(ls *.yaml); do
+        echo $file
+        sed "s/@{registry_id}/${REGISTRY_ID}/g" ${file} >.${file}_0
+        sed "s/@{tag}/${TAG}/g" .${file}_0 >.${file}_1
+        sed "s/@{profile}/${PROFILE}/g" .${file}_1 >.${file}
+        rm .${file}_0 .${file}_1
+
+        if [[ ${args[0]} -eq 0 ]]; then
+            kubectl apply -f .${file}
+        else
+            sudo kubectl apply -f .${file}
+        fi
+    done
+}
+start
--- stop.sh
+++ stop.sh
@@ -1,3 +1,40 @@
 #!/bin/bash
 
-sudo kubectl delete namespace blog
+argc=$#
+args=("$@")
+
+function printUsage() {
+    echo "USAGE: /bin/bash stop.sh {target: integer value, 0 (local), 1 (dev), 2 (prod)}"
+}
+
+function init() {
+    if [[ ${argc} -eq 1 ]]; then
+        if [[ ${args[0]} -eq 0 ]]; then
+            echo "Stop local profile services and deployments."
+        elif [[ ${args[0]} -eq 1 ]]; then
+            echo "Stop dev profile services and deployments."
+        elif [[ ${args[0]} -eq 2 ]]; then
+            echo "Stop prod profile services and deployments."
+        else
+            printUsage
+            exit 0
+        fi
+    else
+        printUsage
+        exit 0
+    fi
+}
+init
+
+function stop() {
+    if [[ ${args[0]} -eq 0 ]]; then
+        kubectl delete service svc-for-blog-web-service -n blog
+        kubectl delete deployment deployment-for-blog-web-service -n blog
+    else
+        sudo kubectl delete service svc-for-blog-web-service -n blog
+        sudo kubectl delete deployment deployment-for-blog-web-service -n blog
+    fi
+
+    rm .*.yaml
+}
+stop
